2ac3766648ba3950736c70d6c3383f1693e09fa7
shsh99 <ggg9905@naver.com>
2026-03-30 20:31:37 +0900

    "configfile 추가"

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig(({ command }) => {
   }
 
   return {
-    publicDir: false,
+    publicDir: './webapp/public',
     esbuild: {
       jsx: 'automatic',
       jsxImportSource: 'hono/jsx',
